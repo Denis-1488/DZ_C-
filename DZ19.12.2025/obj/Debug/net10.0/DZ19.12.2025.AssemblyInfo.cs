09615c503885ae530cdc94d48399d1171a213def
Денис Тишковский <dentiskovskij@gmai.com>
2026-01-01 14:39:21 +0200

    домашния

--- DZ19.12.2025/obj/Debug/net10.0/DZ19.12.2025.AssemblyInfo.cs
+++ DZ19.12.2025/obj/Debug/net10.0/DZ19.12.2025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DZ19.12.2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f42a8e030934dc3e53af18135272b41ec9d554e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f93a721235550ec737beab42ea347cf953cb912")]
 [assembly: System.Reflection.AssemblyProductAttribute("DZ19.12.2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DZ19.12.2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ DZ29.12.2025/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+  
+    delegate bool NumberFilter(int number);
+
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 8, 13, 21, 34, 55, 89 };
+
+        Show("Четные", numbers, IsEven);
+        Show("Нечетные", numbers, IsOdd);
+        Show("Простые", numbers, IsPrime);
+        Show("Фибоначчи", numbers, IsFibonacci);
+    }
+
+   
+    static void Show(string title, int[] array, NumberFilter filter)
+    {
+        Console.Write(title + ": ");
+        foreach (int n in array)
+            if (filter(n))
+                Console.Write(n + " ");
+        Console.WriteLine();
+    }
+
+    // Четные
+    static bool IsEven(int n) => n % 2 == 0;
+
+    // Нечетные
+    static bool IsOdd(int n) => n % 2 != 0;
+
+    // Простые числа
+    static bool IsPrime(int n)
+    {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+
+    static bool IsFibonacci(int n)
+    {
+        int a = 0, b = 1;
+        while (b < n)
+        {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return n == b || n == 0;
+    }
+}
