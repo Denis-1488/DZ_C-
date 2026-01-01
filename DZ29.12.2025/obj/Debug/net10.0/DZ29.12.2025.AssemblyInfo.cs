07cf17007a58920fb47d67a3fa7ff868106ebe66
Денис Тишковский <dentiskovskij@gmai.com>
2026-01-02 00:20:48 +0200

    _

--- DZ29.12.2025/obj/Debug/net10.0/DZ29.12.2025.AssemblyInfo.cs
+++ DZ29.12.2025/obj/Debug/net10.0/DZ29.12.2025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DZ29.12.2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f93a721235550ec737beab42ea347cf953cb912")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09615c503885ae530cdc94d48399d1171a213def")]
 [assembly: System.Reflection.AssemblyProductAttribute("DZ29.12.2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DZ29.12.2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ DZ31.12.2025/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Backpack
+{
+    public string Color { get; set; }
+    public string Brand { get; set; }
+    public string Material { get; set; }
+    public double Weight { get; set; }   // кг
+    public double Volume { get; set; }   // литры
+
+    public List<string> Contents { get; set; }
+
+    public Backpack(string color, string brand, string material,
+                    double weight, double volume)
+    {
+        Color = color;
+        Brand = brand;
+        Material = material;
+        Weight = weight;
+        Volume = volume;
+        Contents = new List<string>();
+    }
+
+    public void AddItem(string item)
+    {
+        Contents.Add(item);
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Цвет: {Color}");
+        Console.WriteLine($"Фирма: {Brand}");
+        Console.WriteLine($"Ткань: {Material}");
+        Console.WriteLine($"Вес: {Weight} кг");
+        Console.WriteLine($"Объем: {Volume} л");
+        Console.WriteLine("Содержимое: " +
+            (Contents.Count > 0 ? string.Join(", ", Contents) : "пусто"));
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Backpack backpack = new Backpack("Черный", "Nike", "Нейлон", 1.2, 25);
+
+        backpack.AddItem("Книга");
+        backpack.AddItem("Ноутбук");
+        backpack.AddItem("Бутылка воды");
+
+        backpack.ShowInfo();
+    }
+}
